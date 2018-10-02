6e24e4460e8341a81594e6f6371782c3ae9a9424
Unknown <s3559660@student.rmit.edu.au>
2018-10-02 19:10:32 +1000

    Beginnings of process association and applying shellcheck suggestions

--- script.sh
+++ script.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
+#backgroundMonitorFunction(string processName, int monitorType) where monitorType = 0 for CPU and 1 for memory
+backgroundMonitorFunction()
+{
+    processName=$1
+    monitorType=$2
+    while true
+    do
+        if [ "$monitorType" -eq 0 ]
+        then
+            echo "I'm monitoring the CPU of $processName"
+        fi
+        if [ "$monitorType" -eq 1 ]
+        then
+            echo "I'm monitoring the Memory of $processName"
+        fi
+        sleep 10
+    done
+}
+
 mainMenu()
 {
     clear
-    availableleds=(`ls /sys/class/leds/`)
-    menuitems=("${availableleds[@]}")
+    mapfile -t availableLeds < <(ls /sys/class/leds/)
+    menuitems=("${availableLeds[@]}")
     menuitems+=("Quit")
     num=${#menuitems[@]}
-    quitindex=$num
 
+    (
     cat <<EOM
 Welcome to Led_Konfigurator!
 ============================
 Please select an LED to configure: 
 EOM
     
-    for ((i=1; i<=$num; i++))
+    for ((i=1; i<=num; i++))
     do
-        echo $i: ${menuitems[(($i-1))]}
+        echo $i\) "${menuitems[(($i-1))]}"
     done
     echo "Please enter a number (1-$num) for the led to configure or quit:"
+    ) | more
 
-    read input
-    if [ $input -eq $num ]
+    read -r input
+    if [ "$input" -eq "$num" ]
     then
         exit $?
     fi
@@ -35,6 +55,7 @@ EOM
 manipulateLED()
 {
     clear
+    (
     cat <<EOM
 $ledtomanipulate
 ============================
@@ -47,25 +68,30 @@ What would you like to do with this led?
 6) quit to main menu
 Please enter a number (1-6) for your choice:
 EOM
+    ) | more
 
     #Unsure about brackets after numbers, doesn't fit style of mainMenu()
     #SANITIZE INPUT, LETTERS SHOULDN'T KILL THE SCRIPT
     #also handle input better overall
 
-    read input
-    if [ $input -eq 1 ]
+    read -r input
+    if [ "$input" -eq 1 ]
     then
         turnOnLED
     fi
-    if [ $input -eq 2 ]
+    if [ "$input" -eq 2 ]
     then
         turnOffLED
     fi
-    if [ $input -eq 3 ]
+    if [ "$input" -eq 3 ]
     then
         associateWithSystemEvent
     fi
-    if [ $input -eq 6 ]
+    if [ "$input" -eq 4 ]
+    then
+        associateWithProcess
+    fi
+    if [ "$input" -eq 6 ]
     then
         mainMenu
     fi
@@ -73,40 +99,42 @@ EOM
 
 turnOnLED()
 {
-    echo 1 >/sys/class/leds/$ledtomanipulate/brightness
+    echo 1 >/sys/class/leds/"$ledtomanipulate"/brightness
     manipulateLED
 }
 
 turnOffLED()
 {
-    echo 0 >/sys/class/leds/$ledtomanipulate/brightness
+    echo 0 >/sys/class/leds/"$ledtomanipulate"/brightness
     manipulateLED
 }
 
 associateWithSystemEvent()
 {
     clear
+
+    IFS=" "
+    read -r -a availableEvents <<< "$(cat /sys/class/leds/"$ledtomanipulate"/trigger)"
+    eventMenuitems=("${availableEvents[@]}")
+    eventMenuitems+=("Quit to previous menu")
+    eventNum=${#eventMenuitems[@]}
+
+    (
     cat <<EOM
 Associate Led with a system Event
 =================================
 Available events are: 
 ---------------------
 EOM
-
-    availableEvents=(`cat /sys/class/leds/$ledtomanipulate/trigger`)
-    eventMenuitems=("${availableEvents[@]}")
-    eventMenuitems+=("Quit to previous menu")
-    eventNum=${#eventMenuitems[@]}
-    eventQuitindex=$eventNum
-
-    for ((i=1; i<=$eventNum; i++))
+    for ((i=1; i<=eventNum; i++))
     do
-        echo $i: ${eventMenuitems[(($i-1))]}
+        echo $i: "${eventMenuitems[(($i-1))]}"
     done
     echo "Please select an option (1-$eventNum):"
+    ) | more
 
-    read input
-    if [ $input -eq $eventNum ]
+    read -r input
+    if [ "$input" -eq "$eventNum" ]
     then
         manipulateLED
     fi
@@ -117,7 +145,64 @@ EOM
 
 associateWithSystemEventAction()
 {
-    echo $eventToAssociate >/sys/class/leds/$ledtomanipulate/trigger
+    echo "$eventToAssociate" >/sys/class/leds/"$ledtomanipulate"/trigger
+    manipulateLED
+}
+
+associateWithProcess()
+{
+    clear
+    cat <<EOM
+Associate LED with the performance of a process
+------------------------------------------------
+Please enter the name of the program to monitor(partial names are ok):
+EOM
+
+    read -r procName
+    procNum=$(pgrep -c "$procName")
+    echo "$procNum"
+
+    if [ "$procNum" -eq 0 ]
+    then
+        echo "BAD BAD NOT GOOD"
+    fi
+    if [ "$procNum" -gt 1 ]
+    then
+        chooseProcess
+    fi
+
+    pickAssociation
+}
+
+chooseProcess()
+{
+    clear
+
+    cat <<EOM
+Name Conflict
+-------------
+I have detected a name conflict. Do you want to monitor: 
+EOM
+
+    read -r
+}
+
+pickAssociation()
+{
+    clear
+
+    echo "Do you wish to 1) monitor memory or 2) monitor cpu? [enter memory or cpu]:"
+    read -r monitorType
+
+    if [ "$monitorType" == "memory" ]
+    then
+        (backgroundMonitorFunction "$procName" "1") &
+    fi
+    if [ "$monitorType" == "cpu" ]
+    then
+        (backgroundMonitorFunction "$procName" "0") &
+    fi
+
     manipulateLED
 }
 
